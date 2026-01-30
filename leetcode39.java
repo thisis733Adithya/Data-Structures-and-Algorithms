261fbf7d8cff40d0399d92c95cda369d4853008f
Adithya KrishnaMurthy <adithyaMelige@gmail.com>
2026-01-30 19:03:32 +0530

    Combination Sum
    This problem uses Backtracking Approach

--- leetcode39.java
+++ leetcode39.java
@@ -1,17 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class leetcode39 {
+
+    public static void main(String[] args) {
+        List<List<Integer>> result = combinationSum(new int[]{2,3,6,7}, 7);
+        System.out.println(result);
+    }
 
 // 39. Combination Sum
 // Medium
 // Topics
 // premium lock icon
 // Companies
-// Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+// Given an array of distinct integers candidates and a target integer target,
+//  return a list of all unique combinations of candidates where the chosen numbers sum to target.
+//  You may return the combinations in any order.
 
-// The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+// The same number may be chosen from candidates an unlimited number of times. 
+// Two combinations are unique if the frequency of at least one of the chosen numbers is different.
 
-// The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+// The test cases are generated such that the
+//  number of unique combinations that sum up to target is less than 150 combinations for the given input.
 
  
 
@@ -31,8 +41,6 @@ public class leetcode39 {
 
 // Input: candidates = [2], target = 1
 // Output: []
- 
-
 // Constraints:
 
 // 1 <= candidates.length <= 30
@@ -40,8 +48,33 @@ public class leetcode39 {
 // All elements of candidates are distinct.
 // 1 <= target <= 40
 
-public List<List<Integer>> combinationSum(int[] candidates, int target) {
-   
+public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+   List<List<Integer>> result = new ArrayList<>();
+   CombSum(candidates, 0, target, new ArrayList<>(), result , 0);
+
+   return result;
 
     }
+private static void CombSum(int []candidates, int curSum , int target,List<Integer> list, List<List<Integer>> result,
+    int start
+)
+    {
+        if(curSum > target)
+        {
+            return;
+        }
+        if(curSum == target)
+        {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        for(int i=start; i<candidates.length; i++)
+        {
+            list.add(candidates[i]);
+            CombSum(candidates, curSum+candidates[i] , target, list, result, i);
+            list.remove(list.size()-1);
+        }
+    }
 }
+
